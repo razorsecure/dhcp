c25c9fccc97ab7ffec7f16092e698c7914b11d42
Daniel Smith <daniel@razorsecure.com>
2024-05-07 10:17:48 +0100

    applying patch from https://github.com/isc-projects/dhcp/commit/6c7e61578b1b449272dbb40dd8b98d03dad8a57a to fix newer gcc build issues
    Signed-off-by: Daniel Smith <daniel@razorsecure.com>

--- common/discover.c
+++ common/discover.c
@@ -42,8 +42,8 @@
 struct interface_info *interfaces, *dummy_interfaces, *fallback_interface;
 int interfaces_invalidated;
 int quiet_interface_discovery;
-u_int16_t local_port;
-u_int16_t remote_port;
+u_int16_t local_port = 0;
+u_int16_t remote_port = 0;
 u_int16_t relay_port = 0;
 int dhcpv4_over_dhcpv6 = 0;
 int (*dhcp_interface_setup_hook) (struct interface_info *, struct iaddr *);
